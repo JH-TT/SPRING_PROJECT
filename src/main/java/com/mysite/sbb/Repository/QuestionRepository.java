586e7c8a686e12e1ee74285a3c874b83d0d23274
JH-TT <leejongho9803@gmail.com>
2022-12-09 03:24:17 +0900

    fix(question_detail) - 대댓글 N+1문제 해결, Batchsize 설정

--- src/main/java/com/mysite/sbb/Repository/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/Repository/QuestionRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
@@ -15,6 +17,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     QuestionDTO findBySubject(String subject);
     QuestionDTO findBySubjectAndContent(String subject, String content);
 
+    // N+1 문제를 해결하기 위함
+    @Query("select q from Question q join fetch q.answerList where :id = q.id")
+    Optional<Question> findById(Integer id);
     // 제목에 특정 문자열이 포함되어 있는 데이터 조회.
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
