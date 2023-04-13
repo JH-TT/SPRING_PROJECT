858b68a0589ca98165c1e6cb8b27fa36104fc345
JH-TT <leejongho9803@gmail.com>
2023-04-13 19:09:58 +0900

    update(siteuser) - 유저이름제한 삭제, 생성 메서드 추가
    - 소셜로그인은 유저이름에 제약을 받지않고 임시로 만들 수 있도록 하기위함

--- src/main/java/com/mysite/sbb/Model/SiteUser.java
+++ src/main/java/com/mysite/sbb/Model/SiteUser.java
@@ -19,10 +19,9 @@ public class SiteUser extends BaseTimeEntity {
     @Column(name = "siteuser_id")
     private Long id;
 
-    @NotBlank @Length(min = 2, max = 10)
-    @Pattern(regexp = "^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{1,10}$") // 한글, 영문, 숫자만 가능하며 2-10자리 가능
+    @NotBlank
     @Column(unique = true)
-    private String username;
+    private String username; // 소셜 로그인은 username에 제약받지 않도록 구현(일반 회원가입으로는 만들 수 없는 아이디로 생성해서 구분하기 위함)
 
     private String password;
 
@@ -37,12 +36,25 @@ public class SiteUser extends BaseTimeEntity {
     @Column(nullable = false)
     private boolean emailCheck = false; // 이메일 인증관련
 
+    //========= 생성 메서드 ==========
+
     public SiteUser(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
         role = UserRole.USER;
         isNameChange = true;
+    }
+
+    @Builder
+    public SiteUser(Long id, String username, String password, String email, boolean isNameChange, boolean emailCheck, UserRole role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.isNameChange = isNameChange;
+        this.emailCheck = emailCheck;
+        this.role = role;
     }
 
     @Builder
