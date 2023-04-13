ceef69b47d762642f4a259939b8cecf2843ff6ca
JH-TT <leejongho9803@gmail.com>
2023-04-13 19:05:49 +0900

    amend(login) - 소셜 로그인 로직 수정
    - 기존 회원이 존재하면 수정날짜 정보만 업데이트하도록 추가, 임시 아이디 uuid로 변경
    - providerId가 어떤 소셜로그인이냐에 따라 없거나 달라서 그냥 uuid로 진행

--- src/main/java/com/mysite/sbb/OAuth/PrincipalOauth2UserService.java
+++ src/main/java/com/mysite/sbb/OAuth/PrincipalOauth2UserService.java
@@ -2,17 +2,10 @@ package com.mysite.sbb.OAuth;
 
 import com.mysite.sbb.DTO.OAuthAttributes;
 import com.mysite.sbb.DTO.SessionUser;
-import com.mysite.sbb.DTO.SiteUserDTO;
-import com.mysite.sbb.Enum.UserRole;
-import com.mysite.sbb.Model.PrincipalDetails;
 import com.mysite.sbb.Model.SiteUser;
-import com.mysite.sbb.OAuth.userinfo.GoogleUserInfo;
-import com.mysite.sbb.OAuth.userinfo.NaverUserInfo;
-import com.mysite.sbb.OAuth.userinfo.OAuth2UserInfo;
 import com.mysite.sbb.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -24,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -39,7 +31,6 @@ public class PrincipalOauth2UserService implements OAuth2UserService<OAuth2UserR
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-        OAuth2UserInfo oAuth2UserInfo = null;
         // 플랫폼 (구글, 네이버, 카카오, 깃허브 등등...)
         String provider = userRequest.getClientRegistration().getRegistrationId();
         // OAuth2 로그인 진행 시 키가 되는 필드 값(PK)
@@ -48,16 +39,24 @@ public class PrincipalOauth2UserService implements OAuth2UserService<OAuth2UserR
         OAuthAttributes attributes = OAuthAttributes.of(provider, userNameAttributeName, oAuth2User.getAttributes());
 
         // 임시 닉네임 만들기
-        String providerId = oAuth2UserInfo.getProviderId();
-        String username = provider + "_" + providerId;
+        String uuid = UUID.randomUUID().toString().substring(0, 10);
+        String username = provider + "_" + uuid;
         attributes.updateName(username);
 
-        SiteUser user = userRepository.findByemail(oAuth2UserInfo.getEmail()).orElse(attributes.toEntity());
-        user.emailVerifiedSuccess(); // 소셜 로그인은 이메일 인증이 필요없음
+        // attributes -> 로그인 성공후 유저 데이터가 담긴 상태
+        SiteUser user = saveOrUpdate(attributes);
         httpSession.setAttribute("user", new SessionUser(userRepository.save(user)));
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
                 attributes.getAttributes(),
                 attributes.getNameAttributeKey());
+    }
+
+    // 소셜로그인시 기존 회원이 존재하면 수정날짜 정보만 업데이트하여 기존의 데이터는 그대로 보존
+    private SiteUser saveOrUpdate(OAuthAttributes attributes) {
+        SiteUser siteUser = userRepository.findByemail(attributes.getEmail()).orElse(attributes.toEntity());
+        // SNS 로그인은 이메일 인증 패스
+        siteUser.emailVerifiedSuccess();
+        return userRepository.save(siteUser);
     }
 }
