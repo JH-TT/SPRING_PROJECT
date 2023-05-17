a21e116f7d8219b61bcbad92da7b854aeb7095b4
JH-TT <leejongho9803@gmail.com>
2023-05-18 03:16:58 +0900

    test(question) - 단위 테스트 추가(생성하기)

--- src/main/java/com/mysite/sbb/api/QuestionApiController.java
+++ src/main/java/com/mysite/sbb/api/QuestionApiController.java
@@ -2,12 +2,19 @@ package com.mysite.sbb.api;
 
 import com.mysite.sbb.DTO.QuestionDTO;
 import com.mysite.sbb.DTO.QuestionListDTO;
+import com.mysite.sbb.DTO.SessionUser;
+import com.mysite.sbb.Model.Question;
+import com.mysite.sbb.Model.SiteUser;
+import com.mysite.sbb.QuestionForm;
 import com.mysite.sbb.Service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +22,12 @@ public class QuestionApiController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/getlist")
+    @PostMapping("/api/questions")
+    public Long addQuestion(@RequestBody QuestionForm questionForm, String email) {
+        return questionService.create(questionForm.getSubject(), questionForm.getContent(), email);
+    }
+
+    @GetMapping("/questiongetlist")
     public Page<QuestionListDTO> getQuestionList() {
         return questionService.getListV1(0, "");
     }
@@ -23,5 +35,14 @@ public class QuestionApiController {
     @GetMapping("/question/details/{id}")
     public QuestionDTO getQuestion(@PathVariable("id") Long id) {
         return questionService.getQuestion(id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/question/voteCheck/{id}")
+    public boolean QuestionVoteCheck(@PathVariable("id") Long id, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        SessionUser user = (SessionUser) session.getAttribute("user");
+        System.out.println("여기 지남");
+        return questionService.checkLiked(id, user.getName());
     }
 }
--- src/test/java/com/mysite/sbb/Controller/QuestionControllerTest.java
+++ src/test/java/com/mysite/sbb/Controller/QuestionControllerTest.java
@@ -1,14 +1,15 @@
 package com.mysite.sbb.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysite.sbb.Model.Question;
 import com.mysite.sbb.Model.SiteUser;
+import com.mysite.sbb.QuestionForm;
 import com.mysite.sbb.Repository.QuestionRepository;
+import com.mysite.sbb.Repository.UserRepository;
 import com.mysite.sbb.Service.QuestionService;
 import com.mysite.sbb.Service.UserService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,10 +17,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -32,7 +39,7 @@ public class QuestionControllerTest {
     private WebApplicationContext context;
 
     @Autowired
-    private QuestionService questionService;
+    private ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
 
     @Autowired
     private QuestionRepository questionRepository;
@@ -40,23 +47,54 @@ public class QuestionControllerTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @BeforeEach // 테스트 실행 전 실행하는 메서드
     public void mockMvcSetUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
+        questionRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
-    @AfterEach // 테스트 실행 후 실행하는 메서드
-    public void cleanUp() {
-        questionRepository.deleteAll();
+    @DisplayName("addQuestion : 게시글 추가에 성공한다.")
+    @Test
+    public void addQuestion() throws Exception {
+        // given
+        final String url = "/api/questions";
+        final String title = "title";
+        final String content = "content";
+        final QuestionForm questionForm = new QuestionForm(title, content);
+        SiteUser siteUser = userService.create("ttt", "ttt@test.com", "1234");
+
+        // 객체 json으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(questionForm);
+
+        // when
+        // 설정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody)
+                .param("email", siteUser.getEmail()));
+
+        // then
+        result.andExpect(status().isOk());
+
+        List<Question> questions = questionRepository.findAll();
+
+        // 1개가 존재하고 제목과 내용이 같다.
+        assertThat(questions.size()).isEqualTo(1);
+        assertThat(questions.get(0).getSubject()).isEqualTo(title);
+        assertThat(questions.get(0).getContent()).isEqualTo(content);
     }
 
     @DisplayName("특정 게시글을 가져온다.")
     @Test
     public void getQuestion() throws Exception {
         // given
-        SiteUser siteUserDTO = userService.create("ttt", "ttt@test.com", "1234");
-        Question savedQuestion = questionRepository.save(new Question("하이요", "제목", siteUserDTO));
+        SiteUser siteUser = userService.create("ttt", "ttt@test.com", "1234");
+        Question savedQuestion = questionRepository.save(new Question("하이요", "제목", siteUser));
         final String url = "/question/details/" + savedQuestion.getId();
 
         // when
